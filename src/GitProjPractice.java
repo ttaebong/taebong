a823640966e3ca554e94b05dc48d00c10c017ab7
태헌 <leeth6540@gmail.com>
2018-01-23 13:46:41 +0900

    숫자를입력하세요  , 숫자를 또 입력하세요 ,입력한숫자

--- src/GitProjPractice.java
+++ src/GitProjPractice.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 
 public class GitProjPractice {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Scanner sc =new Scanner(System.in);
 		System.out.println("안녕?");
 
+		
+		//업적
+		System.out.println("숫자를 입력하세요 ");
+		int left = sc.nextInt();
+		System.out.println("숫자를 또 입력하세요 ");
+		int right =sc.nextInt();
+	
+		
+		//출력 
+		System.out.println("입력한 숫자" + left + "," + right);
 	}
 
 }
